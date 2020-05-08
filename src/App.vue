67ab6e350aa88492ab88ed7c7642276b146a507d
Archisman Sahoo <archismansahoo8@gmail.com>
2020-05-08 23:38:28 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Superman's Vue.js App"/>
+    <HelloWorld msg="Welcome to Archisman's Vue.js App"/>
   </div>
 </template>
 
