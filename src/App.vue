18215de88d0ff0559c6f1d28d41c1107a8fc9159
archisman <archisman.sahoo@rakuten.com>
2020-05-14 15:04:46 +0530

    New text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to BATMAN's Vue.js App 2.0"/>
+    <HelloWorld msg="Welcome to BATMAN's Vue.js App 3.0"/>
   </div>
 </template>
 
